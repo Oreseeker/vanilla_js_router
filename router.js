cbfb9d05bd8386217dfa3b8449e3af73e8515be6
Oreseeker <veniaminamvrosov@g,ail.com>
2021-08-09 09:42:49 +0300

    Route hashing

--- router.js
+++ router.js
@@ -1,12 +1,30 @@
 /* Структура route = { path, component, title, children } */
 
-const clear = containerEl => {
+function clear(containerEl) {
 	while (containerEl.firstChild) containerEl.firstChild.remove();
-};
+}
 
-const openRouteComponent = (route, data) => {
+function openRouteComponent(path, data) {
 	/* this должен ссылаться на Router */
-	let routeTree = getRouteTree(route, this.routes);
+
+	/* Если мы заходили по какому-то маршруту, то мы уже сохранили
+	   дерево маршрутов, нет смысла его строить заново, воспользуеммся
+	   сохраненным результатом */
+	let routeTree = (() => {
+		const hashedRoute = this.hashedRoutes[path];
+		if (hashedRoute) {
+			console.log(`Extracting hashed route`);
+			return this.hashedRoutes[path];
+		}
+		return this.getRouteTree(path, this.routes);
+	})();
+
+	/* Если нашелся маршрут, то сохраняем его дерево на случай,
+	   если мы захотим еще раз перейти на тот же маршрут.
+	   Тогда при последующем запросе мы не будем искать и строить дерево
+	   заново, а воспользуемся сохраненным результатом. */
+	this.hashedRoutes[path] = routeTree;
+
 	const lastRouteIndex = routeTree.length - 1;
 	let containerEl = this.baseApp.routerViewEl;
 	routeTree.forEach((rt, index) => {
@@ -24,81 +42,80 @@ const openRouteComponent = (route, data) => {
 			dataKeys.forEach(key => componentEl.key = data[key]);
 		}
 	});
-};
+}
 
-const getRouteTree = (route, routes) => {
-	if (!route) {
-		console.error('Wrong route value:', route);
+function getRouteTree(path, routes) {
+	if (!path) {
+		console.error('Wrong path value:', path);
 		return null;
 	}
 	let currentSearchPath = [];
 	let isFound = false;
-	const scanRouteTree = (rt, rts) => {
+	const scanRouteTree = (pth, rts) => {
 		for (let i = 0; i < rts.length; i++) {
 			if (isFound) break;
 			currentSearchPath.push(rts[i]);
-			if (rts[i].path === rt.path) {
+			if (rts[i].path === pth) {
 				isFound = true;
 				break;
 			}
 			const hasChildren = rts[i].children && rts[i].children.length;
 			if (hasChildren) {
-				scanRouteTree(rt, rts[i].children);
+				scanRouteTree(path, rts[i].children);
 			}
 			if (!isFound) currentSearchPath.pop();
 		}
 	};
-	scanRouteTree(route, routes);
+	scanRouteTree(path, routes);
 	return currentSearchPath;
-};
+}
 
-const to = (route, title, data) => {
-	this.previousRoute = this.currentRoute;
-	this.currentRoute = route;
-	window.history.pushState(data, title, route.path);
-	this.openRouteComponent(route, data);
-
-};
+function to(path, title, data) {
+	this.previousPath = this.currentPath;
+	this.currentPath = path;
+	window.history.pushState(data, title, path);
+	this.openRouteComponent(path, data);
+}
 
 function Router(routes) {
-	/* Функция-конструктор. Принимает в качестве аргументов baseApp -
-	   ссылкк на основной компонент приложения и routes - конфигурация раутов
-	   для Web-приложения */
+	/* Функция-конструктор. Принимает в качестве аргументов
+	   routes - конфигурация раутов для Web-приложения */
+	console.log(`routes`, routes);
 	this.routes = routes;
-	this.currentRoute = '/';
-	this.previousRoute = null;
-	this.clear = clear;
-	this.to = to;
-	this.openRouteComponent = openRouteComponent;
+	this.currentPath = '/';
+	this.previousPath = null;
+	this.hashedRoutes = {};
+	this.getRouteTree = getRouteTree.bind(this);
+	this.clear = clear.bind(this);
+	this.to = to.bind(this);
+	this.openRouteComponent = openRouteComponent.bind(this);
 }
 
 const routes = [
 	{
 		path: '/',
-		title: 'root',
-		component: 'Title',
-		children: [
-			{
-				path: '/users/',
-				title: 'users',
-				children: [
-					{
-						path: '/users/1',
-						title: 'tasks'
-					}
-				]
-			},
-			{
-				path: '/demo',
-				title: 'demo'
-			}
-		]
+		component: 'w-main',
+		title: 'Main'
 	},
 	{
-		path: '/root2'
+		path: '/catalog',
+		component: 'w-catalog',
+		title: 'Catalog'
 	},
 	{
-		path: '/root3'
+		path: '/about-us',
+		component: 'w-about-us',
+		title: 'About us'
+	},
+	{
+		path: '/feedback',
+		component: 'w-feedback',
+		title: 'Feedback'
+	},
+	{
+		path: '/catalog/:id:/',
+		component: 'w-product',
+		title: 'Product'
 	}
 ];
 
